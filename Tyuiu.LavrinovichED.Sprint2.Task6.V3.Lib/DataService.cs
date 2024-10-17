9b16e5374e33fe2705229cb500fe1c850f420253
Tyuiu.lavrinoviched@outlook.com <Tyuiu.lavrinoviched@outlook.com>
2024-10-17 21:17:09 +0500

    Task6

--- Tyuiu.LavrinovichED.Sprint2.Task6.V3.Lib/DataService.cs
+++ Tyuiu.LavrinovichED.Sprint2.Task6.V3.Lib/DataService.cs
@@ -8,13 +8,13 @@ namespace Tyuiu.LavrinovichED.Sprint2.Task6.V3.Lib
         {
             switch (value)
             {
-                case 1: return "Понедельник";
-                case 2: return "Вторник";
-                case 3: return "Среда";
-                case 4: return "Четверг";
-                case 5: return "Пятница";
-                case 6: return "Суббота";
-                case 7: return "Воскресенье";
+                case 1: return "понедельник";
+                case 2: return "вторник";
+                case 3: return "среда";
+                case 4: return "четверг";
+                case 5: return "пятница";
+                case 6: return "суббота";
+                case 7: return "воскресенье";
                 default: throw new ArgumentException("$ День в недели должен быть введен от 1 до 7");
             }
         }
